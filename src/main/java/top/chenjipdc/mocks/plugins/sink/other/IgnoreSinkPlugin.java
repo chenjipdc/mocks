5ef785151e27823b168736980eff01624c95ba15
chenji <chenjipdc@gmail.com>
2023-06-26 11:52:27 +0800

    feat: 新增ignore sink插件，可用于忽略掉sink配置

---
+++ src/main/java/top/chenjipdc/mocks/plugins/sink/other/IgnoreSinkPlugin.java
@@ -0,0 +1,23 @@
+package top.chenjipdc.mocks.plugins.sink.other;
+
+import com.google.auto.service.AutoService;
+import lombok.extern.slf4j.Slf4j;
+import top.chenjipdc.mocks.plugins.SinkPlugin;
+import top.chenjipdc.mocks.plugins.sink.AbstractSinkPlugin;
+
+import java.util.Map;
+
+@Slf4j
+@AutoService(SinkPlugin.class)
+public class IgnoreSinkPlugin extends AbstractSinkPlugin {
+
+    @Override
+    public String type() {
+        return "ignore";
+    }
+
+    @Override
+    public void sink(Map<String, Object> values) {
+
+    }
+}
